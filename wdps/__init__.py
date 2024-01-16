99faa4b846f1bd69c76aca3e06a4ab07236055b0
galarcon-ccg <galarcon@ccg.unam.mx>
2024-01-16 14:55:50 -0600

    add templates and static flow data

--- wdps/__init__.py
+++ wdps/__init__.py
@@ -1,13 +1,22 @@
+import os
+import ssl
 from flask import Flask
 from .sites.routes import wdps
 from .apis.routes import api
 from flask_cors import CORS
 from sgqlc.endpoint.http import HTTPEndpoint
 
+ssl._create_default_https_context = ssl._create_unverified_context
+
 def create_app():
     app = Flask(__name__)
-    
+    CORS(app)
+    #GraphQL client configuration
+    gql_url = os.environ.get("GQL_SERVICE")
+    browser_url = os.environ.get("REGULONDB_BROWSER")
+    gql_service = HTTPEndpoint(gql_url)
+    #Blueprints configuration
     app.register_blueprint(wdps)
     app.register_blueprint(api)
-        
+    
     return app
--- wdps/sites/routes.py
+++ wdps/sites/routes.py
@@ -1,7 +1,22 @@
-from flask import Blueprint
+import os
+from flask import Blueprint, render_template, send_file
 
-wdps = Blueprint('wdps', __name__, url_prefix='/wdps')
+wdps = Blueprint('wdps', __name__, url_prefix='/wdps', template_folder="./wdps/templates", static_folder="./wdps/static")
 
 @wdps.route("/")
 def index():
-    return "welcome RegulonDB-wdps"
+    return render_template('index.html')
+
+#Statics
+@wdps.route('/static/<type>/<file_name>')
+def static_file(type, file_name):
+    ruta_script = __file__
+    dir_absoluto = os.path.dirname(os.path.abspath(ruta_script))
+    dir = dir_absoluto+'/wdps/static/'+type+'/'
+    path = os.path.join(dir, file_name)
+    print(path)
+    if os.path.exists(path):
+        return send_file(path, as_attachment=True, download_name=file_name)
+    else:
+        print("file ",file_name," no found in route ",path)
+        return "file no found"
